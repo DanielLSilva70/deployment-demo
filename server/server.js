1c3288073ec468915495f6ec9a39ec40b267380e
Daniel Silva <daniel@Daniels-MacBook-Air.local>
2022-02-09 12:23:57 -0600

    fixing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
@@ -13,4 +14,4 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4000
 
-app.listen(port, () => console.log(`Server Running On 4000`))
+app.listen(port, () => console.log(`Server Running On ${port}`))
